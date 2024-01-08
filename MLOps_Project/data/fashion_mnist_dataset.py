e8583eaf75ce26882e8d7766a2b3331789381c78
MapleWarrior <michaelharborg@gmail.com>
2024-01-08 16:13:27 +0100

    Update fashion_mnist_dataset.py

--- MLOps_Project/data/fashion_mnist_dataset.py
+++ MLOps_Project/data/fashion_mnist_dataset.py
@@ -14,46 +14,47 @@ class FashionMNISTDataset(Dataset):
         self.config = config
         self.images = data[0] # N x 1 x H x W
         self.labels = data[1] # N
-        pdb.set_trace()
-        self.transformations = Compose(self.__get_transformations())
+        self.__get_transformations()
         
     def __get_transformations(self):
 
         rotate = RandomRotation(degrees=self.config.transformations.rotation.degrees,
                                 interpolation=InterpolationMode.BILINEAR)
-        
         scale = None
         transforms = {'rotation': rotate}
-        return [transform for name, transform in transforms.items() if self.config['transformations'][f'{name}']['use']]
+
+        augmentations = [transform for name, transform in transforms.items() if self.config['transformations'][f'{name}']['use']]
+        if len(augmentations):
+            self.transformations = Compose(augmentations)
+        else:
+            self.transformations = None
+
 
     def __getitem__(self, index) -> any:
-        image = self.transformations(self.images[index])
-        label = torch.tensor(self.labels[index], dtype=torch.long)
+        image = self.transformations(self.images[index]) if self.transformations else self.images[index]
+        label = self.labels[index]
         return image, label
 
     def __len__(self):
         return len(self.labels)
 
 
-@hydra.main(version_base=None, config_path='../../configs', config_name='config')
-def get_train_dataset(cfg : DictConfig):
-    data = torch.load(os.path.join(cfg.data.processed_dir, 'train.pt'))
-    train_dataset = FashionMNISTDataset(data, cfg.data.train)
-    train_dataloader = DataLoader(train_dataset, batch_size=cfg.data.batch_size, shuffle=True)
-
-
-@hydra.main(version_base=None, config_path='../../configs', config_name='config')
-def get_test_dataset(cfg):
-    data = torch.load(os.path.join(cfg.data.processed_dir, 'test.pt'))
-    test_dataset = hydra.utils.instantiate(cfg.data.test)
-    test_dataloader = DataLoader(test_dataset, batch_size=cfg.data.batch_size, shuffle=False)
-
+def get_dataset(cfg, split: str = 'train'):
+    shuffle = True if split == 'train' else False
+    data = torch.load(os.path.join(cfg.data.processed_dir, f'{split}.pt'))
+    dataset = FashionMNISTDataset(data, cfg['data'][split])
+    dataloader = DataLoader(dataset, batch_size=cfg.data.batch_size, shuffle=shuffle)
+    return dataloader
 
 @hydra.main(version_base=None, config_path='../../configs', config_name='config')
-def get_val_dataset(cfg):
-    data = torch.load(os.path.join(cfg.data.processed_dir, 'val.pt'))
-    val_dataset = hydra.utils.instantiate(cfg.data.test)
-    val_dataloader = DataLoader(val_dataset, batch_size=cfg.data.batch_size, shuffle=False)
+def main(cfg: DictConfig):
+    train_loader = get_dataset(cfg, split='train')
+    _x = next(iter(train_loader))
+    # pdb.set_trace()
+    # val_loader = get_dataset(cfg, split='val')
+    test_loader = get_dataset(cfg, split='test')
+    _x = next(iter(test_loader))
+    return train_loader, test_loader
 
 if __name__ == '__main__':
-    get_train_dataset()
+    main()
