b4d1515064c478f10f7cef90c9b4d4f4a389cc19
magn2998 <magnus.fjellerup@gmail.com>
2024-01-09 14:32:48 +0100

    Added git test actions and basic test

---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+import os
+_TEST_ROOT = os.path.dirname(__file__)
+_PROJECT_ROOT = os.path.dirname(_TEST_ROOT)
+_PATH_DATA = os.path.join(_PROJECT_ROOT, "Data")
---
+++ tests/test_model.py
@@ -0,0 +1,9 @@
+from MLOps_Project.models.model import ResNet34 
+import torch
+
+def test_train():
+    randomData = torch.rand((10, 1, 28, 28))
+    model = ResNet34()
+    pred = model.forward(randomData)
+
+    assert pred.shape == torch.Size((10, 10)), "Shape of the output of the prediction should be [batchSize, 10]"
