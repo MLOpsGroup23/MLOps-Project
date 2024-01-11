0c52634b9827d5234cbe76af9773634e8f5be8aa
MapleWarrior <michaelharborg@gmail.com>
2024-01-11 10:40:33 +0100

    update training transformations and custom dataset

--- MLOps_Project/data/fashion_mnist_dataset.py
+++ MLOps_Project/data/fashion_mnist_dataset.py
@@ -2,8 +2,9 @@ import torch
 import hydra
 import os
 from torch.utils.data import Dataset, DataLoader
-from torchvision.transforms.v2 import RandomRotation, InterpolationMode, Compose
+from torchvision.transforms.v2 import RandomRotation, Compose
 from omegaconf import DictConfig
+import pdb
 
 class FashionMNISTDataset(Dataset):
     def __init__(self, data, transformations = None) -> None:
@@ -22,17 +23,33 @@ class FashionMNISTDataset(Dataset):
     def __len__(self):
         return len(self.labels)
 
+def compose_transformations(cfg):
+    if cfg is None:
+        return
+    transforms = []
+    options = {'rotation' : RandomRotation}
+    for transform in cfg.keys():
+        if transform in options.keys():
+            transforms.append(options[transform](**cfg[transform]))
+    return Compose(transforms)
+    
+
+
+
 def get_dataset(cfg, split: str = 'train'):
     shuffle = True if split == 'train' else False
     data = torch.load(os.path.join(cfg.data.processed_dir, f'{split}.pt'))
-    dataset = FashionMNISTDataset(data, cfg['data'][split])
+    transformations = compose_transformations(cfg['data'][split]['transformations'])
+    dataset = FashionMNISTDataset(data, transformations)
     dataloader = DataLoader(dataset, batch_size=cfg.data.batch_size, shuffle=shuffle, num_workers=cfg.data.num_workers)
     return dataloader
 
-def get_train_test_dataloaders():
+def get_dataloaders():
     with hydra.initialize(version_base=None, config_path='../../configs'):
         cfg = hydra.compose(config_name='config')
-
+    
     train_loader = get_dataset(cfg=cfg, split='train')
+    val_loader = get_dataset(cfg=cfg, split='val')
     test_loader = get_dataset(cfg=cfg, split='test')
-    return train_loader, test_loader
+    
+    return train_loader, val_loader, test_loader
--- MLOps_Project/train_model.py
+++ MLOps_Project/train_model.py
@@ -3,10 +3,10 @@ from pytorch_lightning import Trainer
 from MLOps_Project.models.model import ResNet34
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, TensorDataset
-from data.fashion_mnist_dataset import get_train_test_dataloaders
+from data.fashion_mnist_dataset import get_dataloaders
 
 # Create custom datasets and dataloaders
-train_dataloader, val_dataloader = get_train_test_dataloaders()
+train_dataloader, val_dataloader, test_dataloader = get_dataloaders()
 
 # Test Training
 model = ResNet34()
