e92b4bb1bc4cb7e7e17d759c0c357c796a44cb05
magn2998 <magnus.fjellerup@gmail.com>
2024-01-12 11:24:55 +0100

    Fixed server to work with model trained on normalized dataset

--- MLOps_Project/endpoints/main.py
+++ MLOps_Project/endpoints/main.py
@@ -45,6 +45,9 @@ def bmp_to_tensor(bmp_data):
     tensor = torch.tensor(list(image.getdata()))
     tensor = tensor.view(1, 28, 28).float()  
 
+    # Normalize to fit dataset
+    tensor = (tensor - torch.mean(tensor))/torch.std(tensor)
+
     return tensor
 
 # ===================== Routes =====================
