a2ff937e905fe19aa676e98f6d222f88b99c8013
Andreas Fiehn <andreas@lyhnefiehn.dk>
2024-01-15 16:34:12 +0100

    Revert "Make a ton of new models"

--- MLOps_Project/train_model.py
+++ MLOps_Project/train_model.py
@@ -1,6 +1,7 @@
 import torch
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import WandbLogger
+from MLOps_Project.models.resnet import ResNet34
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, TensorDataset
 from data.fashion_mnist_dataset import get_dataloaders
